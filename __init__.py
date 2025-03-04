9e0f21da617d3936846f3b2143fc04214ca4a25f
icebergcn777 <125659422+icebergcn777@users.noreply.github.com>
2025-03-04 22:20:36 +0800

    追加

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from .invert_image_node import InvertImageNode
 
 NODE_CLASS_MAPPINGS = {
-    "Invert Image Node Sample": InvertImageNode
-    }
+"Invert Image Node Sample": InvertImageNode
+}
 
-__all__ = ["NODE_CLASS_MAPPINGS"]
+WEB_DIRECTORY = "./js"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "WEB_DIRECTORY"]
 
 '''ここでは、キーとなる文字列"Invert Image Node Sample"がComfyUI内部でのノード名として扱われます。後でUIの「Add Node」メニューにこの名前が現れるわけです。InvertImageNodeクラスは、レジストリを辿って読み込まれていきます'''
